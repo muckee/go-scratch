2bc9ae8875be1950b253d016d6aa20c1c4442826
Muckee <info@joshuaflood.com>
2023-06-23 18:52:52 +0100

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,27 +11,32 @@ import (
 //go:embed public
 var public embed.FS
 
-// Determine whether to use the default port `9223` or the value of the `GOLANG_PORT` environment variable
-var port = ":9223"
-
 func main() {
+
+  // Declare the default port
+  port := ":9223"
+
+  // Attempt to get the port number from the `GOLANG_PORT` environment variable
+  val, portIsSet := os.LookupEnv("GOLANG_PORT")
+
+  // If the `GOLANG_PORT` environment variable is set, assign its value to the `port` variable
+  if portIsSet {
+    port = `:{{ val }}`
+  }
 
   // We want to serve static content from the root of the 'public' directory,
   // but go:embed will create a FS where all the paths start with 'public/...'.
   // Using fs.Sub we "cd" into 'public' and can serve files relative to it.
   publicFS, err := fs.Sub(public, "public")
+
+  // Throw an error if the filesystem cannot be created
   if err != nil {
     log.Fatal(err)
   }
 
-  // If a custom port number is stored in the `GOLANG_PORT` environment variable, then use it instead of the default port number
-  val, portIsSet := os.LookupEnv("GOLANG_PORT")
-
-  if portIsSet {
-    port := `:{{ val }}`
-  }
-
+  // Serve the filesystem under the root path
   http.Handle("/", http.FileServer(http.FS(publicFS)))
 
+  // If the service fails, log the service
   log.Fatal(http.ListenAndServe(port, nil))
 }
