76e98ebeb3b5da47f9936b4aedc6f61fbfd51705
Muckee <32198963+muckee@users.noreply.github.com>
2023-08-28 13:57:05 +0100

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -50,7 +50,7 @@ func main() {
   // If the static content directory exists, assign it as the value of `publicFS`
   if staticContentDirectoryExists {
       publicFS := http.FileServer(http.Dir(fmt.Sprintf(":%s", staticContentDirectory)))
-      httpFS, err := http.StripPrefix(fmt.Sprintf("/:%s/", staticContentDirectory), fs)
+      httpFS := http.StripPrefix(fmt.Sprintf("/:%s/", staticContentDirectory), fs)
   }
 
   // Point the root endpoint at the chosen filesystem
