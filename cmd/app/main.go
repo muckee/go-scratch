588b8ab71d2672c4f01bdac58204f4ced6560543
Muckee <32198963+muckee@users.noreply.github.com>
2023-09-09 05:46:31 +0100

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -24,44 +24,38 @@ func main() {
 
   // Step 1) Determine which port should be used to serve static content
 
-  // Attempt to retrieve the port from env vars
+  // Retrieve the port from the `GOLANG_PORT` environment variable or use `:9123`
   port, portIsSet := os.LookupEnv("GOLANG_PORT")
 
   // If no port has been set via env vars, use `9223` as the fallback port
   if !portIsSet {
-    port = "9223"
+    port = "9123"
   }
 
   // Step 2) Determine the location of the public filesystem
   publicFS, err := fs.Sub(public, "public")
   httpFS := http.FileServer(http.FS(publicFS))
 
-  // Attempt to get the build directory from the `GOLANG_STATIC_CONTENT_DIRECTORY` environment variable
+  // Get the path of the static content directory from the `GOLANG_STATIC_CONTENT_DIRECTORY` environment variable or use `/static`
   staticContentDirectory, staticContentDirectoryIsSet := os.LookupEnv("GOLANG_STATIC_CONTENT_DIRECTORY")
-
-  // If the `GOLANG_STATIC_CONTENT_DIRECTORY` environment variable is not set, use the default static content directory
   if !staticContentDirectoryIsSet {
-    staticContentDirectory = "static"
+    staticContentDirectory = "/static"
   }
 
-  // Check if the static content directory exists
+  // If the static content directory exists, assign it as the public directory
   staticContentDirectoryExists, err := exists(staticContentDirectory)
-
-  // If the static content directory exists, assign it as the value of `publicFS`
   if staticContentDirectoryExists {
-      httpFS = http.FileServer(http.Dir(fmt.Sprintf(":%s", staticContentDirectory)))
-      // httpFS = http.StripPrefix(fmt.Sprintf("/:%s/", staticContentDirectory), publicFS)
+      httpFS = http.FileServer(http.Dir(fmt.Sprintf("%s", staticContentDirectory)))
   }
 
-  // Point the root endpoint at the chosen filesystem
+  // Handle all requests
   http.Handle("/", httpFS)
 
-  // Throw an error if the filesystem cannot be created
+  // Check for handling errors
   if err != nil {
     log.Fatal(err)
   }
 
   // Serve the static content
-  // The return value of the `http.ListenAndServe()` command is always logged as a fatal error
   log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
